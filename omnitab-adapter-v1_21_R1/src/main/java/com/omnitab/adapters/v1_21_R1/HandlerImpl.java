15aebdb87cba2118a384bbb3f98bad724080af4a
GamingOP69 <samratkafle36@gmail.com>
2026-03-03 14:55:54 +0545

    Elite Fix: Resolved syntax error in v1_21_R1 adapter (missing brace)

--- omnitab-adapter-v1_21_R1/src/main/java/com/omnitab/adapters/v1_21_R1/HandlerImpl.java
+++ omnitab-adapter-v1_21_R1/src/main/java/com/omnitab/adapters/v1_21_R1/HandlerImpl.java
@@ -34,6 +34,8 @@ public class HandlerImpl implements TablistHandler {
     @Override
     public void onPlayerQuit(@NotNull Player player) {
         // No version-specific cleanup needed for 1.21.x yet
+    }
+
     @Override
     public void updateVisibility(@NotNull Player viewer, @NotNull Player target, boolean visible) {
         if (visible) {
